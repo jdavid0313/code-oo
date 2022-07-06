fd0a1a4432a46038b616cadefed502e64bdcd2b8
John David <jdavid@ncees.org>
2022-07-06 15:39:31 -0400

    Breadcrumb update

--- start/manage/delete.php
+++ start/manage/delete.php
@@ -14,9 +14,21 @@ $ship = $shipLoader->findOneById($id);
 ?>
 
 <div class='container'>
-    <a href="show.php?id=<?php echo $id?>"> Back to previous page </a>
+
+    <ul class='breadcrumb'>
+        <li><a href="index.php">Manage Ships</a></li>
+        <li><a href="show.php?id=<?php echo $id?>"><?php echo $ship->getName(); ?> Details </a></li>
+        <li>Delete</li>
+    </ul>
 
     <h2> Are you sure you want to delete the <?php echo $ship->getName();?> ship? </h2>
+
+    <br>
+
+    <a href="delete.php?id=<?php echo $id ?>"><button type="button" class="btn btn-secondary btn-lg">Yes, Delete</button></a>
+    <a href="show.php?id=<?php echo $id ?>"><button type="button" class="btn btn-secondary btn-lg">No, Don't Delete</button></a>
+
+
 
 </div>
 
