4076c21eae0584d5b958d4aab032ab289c8b8315
John David <jdavid@ncees.org>
2022-07-19 14:22:36 -0400

    Different button options add #9

--- start/manage/fleets/details.php
+++ start/manage/fleets/details.php
@@ -40,13 +40,18 @@ foreach ($fleetShips as $fleetShipName => $fleetShips):
             <td><?php echo $fleetShip->getShipName();?></td>
             <td><?php echo $fleetShip->getQuantity();?></td>
             <td>
-                <a href="/manage/fleets/update.php?id=<?php echo $fleetShip->getShipId();?>" class="btn btn-success">Update</a>
-                <a href="/manage/fleets/delete.php?id=<?php echo $fleetShip->getShipId();?>" class="btn btn-danger">Delete</a>
+                <a href="/manage/fleets/update.php?id=<?php echo $fleetShip->getId();?>" class="btn btn-success btn-sm">Edit</a>
+                <a href="/manage/fleets/removeShip.php?shipId=<?php echo $fleetShip->getShipId();?>&fleetId=<?php echo $fleetShip->getId();?>" class="btn btn-danger btn-sm">Remove</a>
             </td>
         </tr>
     <?php endforeach;?>
 </table>
 <?php endforeach;?>
+
+<div class='text-center'>
+    <a href="/manage/fleets/update.php?id=<?php echo $fleetShip->getId();?>" class="btn btn-success">Add Ship</a>
+    <a href="/manage/fleets/delete.php?id=<?php echo $fleetShip->getId();?>" class="btn btn-danger">Delete</a>
+</div>
 
 <?php endif;?>
 <?php require '../ships/footer.php';?>
