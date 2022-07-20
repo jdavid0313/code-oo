5f202e724713a236724465c68726cbece68377e5
John David <jdavid@ncees.org>
2022-07-20 08:29:41 -0400

    New edit/update ship in fleet method #9

---
+++ start/manage/fleets/edit.php
@@ -0,0 +1,75 @@
+<?php
+require '../ships/header.php';
+$shipId = isset($_GET['shipId']) ? $_GET['shipId'] : null;
+$fleetId = isset($_GET['fleetId']) ? $_GET['fleetId'] : null;
+$errors = [];
+
+use Service\Container;
+
+$container = new Container($configuration);
+$fleetLoader = $container->getFleetLoader();
+$fleetShip = $fleetLoader->findShipInFleetById($shipId, $fleetId);
+
+if ($fleetShip === null):
+    $breadcrumbItems = [];
+    include '_breadcrumb.php';
+    echo '<h1>Fleet Not Available</h1>';
+else:
+
+$breadcrumbItems = [
+    [
+        'url'=>'/manage/fleets/details.php?id='.$fleetShip->getId(),
+        'name'=> $fleetShip->getName(). ' Fleet',
+    ],
+    [
+        'url'=>'#',
+        'name'=>'Edit'
+    ]
+];
+
+include '_breadcrumb.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $fleetShip->setQuantity(trim($_POST['quantity']));
+
+    if (empty($fleetShip->getQuantity())) {
+        $errors[] = 'Please enter quantity';
+    }  elseif (is_numeric($fleetShip->getQuantity()) === false || ($fleetShip->getQuantity() < 0)) {
+        $errors[] = "Invalid quantity entered";
+    }
+
+    if (empty($errors)){
+        $fleetStorage = $container->getFleetStorage();
+        $fleetStorage->updateShipInFLeet($fleetShip);
+
+        header('Location: /manage/fleets/fleets.php');
+        return;
+    }
+}
+?>
+<h1>Edit</h1>
+
+<div class='row'>
+    <div class="col-lg-3">
+        <ul class="list-group">
+            <?php foreach ($errors as $errmessage):  ?>
+            <li class="list-group-item list-group-item-danger"><?php echo $errmessage ?>
+            </li>
+            <?php endforeach;  ?>
+        </ul>
+    </div>
+</div>
+
+<form method='POST' action='/manage/fleets/edit.php?shipId=<?php echo $fleetShip->getShipId();?>&fleetId=<?php echo $fleetShip->getId();?>'>
+    <label for='quantity'>Quantity</label>
+    <input class='form-control' type='number' id='quantity' name='quantity' value='<?php echo $fleetShip->getQuantity();?>'/>
+    <br>
+    <div class='text-center'>
+        <button type="submit" class="btn btn-success">Submit</button>
+    </div>
+</form>
+
+
+<?php endif;?>
+<?php require '../ships/footer.php';?>
