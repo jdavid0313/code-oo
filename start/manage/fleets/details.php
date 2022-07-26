1c61f83458ab8c62ea34d5d8b044b82645665328
John David <jdavid@ncees.org>
2022-07-26 08:38:11 -0400

    Method name change and type hint add #9

--- start/manage/fleets/details.php
+++ start/manage/fleets/details.php
@@ -1,15 +1,14 @@
 <?php
 require '../ships/header.php';
 $id = isset($_GET['id']) ? $_GET['id'] : null;
-$team = isset($_GET['team']) ? $_GET['team'] : null;
 
 use Service\Container;
 
 $container = new Container($configuration);
 $fleetLoader = $container->getFleetLoader();
 $shipLoader = $container->getShipLoader();
-$ships = $shipLoader->findShipByTeam($team);
 $fleet = $fleetLoader->getFleetById($id);
+$ships = $shipLoader->findShipsByTeam($fleet->getTeam());
 
 if ($fleet === null):
     $breadcrumbItems = [];
@@ -18,7 +17,7 @@ if ($fleet === null):
 else:
     $breadcrumbItems = [
         [
-            'url'=>'/manage/fleets/details.php?id='.$fleet->getId().'&team='.$fleet->getTeam(),
+            'url'=>'/manage/fleets/details.php?id='.$fleet->getId(),
             'name'=> $fleet->getName(). ' Fleet'
         ]
     ];
@@ -36,13 +35,13 @@ else:
         </tr>
     </thead>
     <tbody>
-    <?php foreach ($fleet->getShipFleets() as $ship):?>
+    <?php foreach ($fleet->getShipFleets() as $ship_fleet):?>
         <tr>
-            <td><?php echo $ship->getShip()->getName();?></td>
-            <td><?php echo $ship->getQuantity();?></td>
+            <td><?php echo $ship_fleet->getShip()->getName();?></td>
+            <td><?php echo $ship_fleet->getQuantity();?></td>
             <td>
-                <a href="/manage/fleets/edit.php?shipId=<?php echo $ship->getShip()->getId();?>&fleetId=<?php echo $fleet->getId()?>" class="btn btn-success btn-sm">Edit</a>
-                <a href="/manage/fleets/removeShip.php?shipId=<?php echo $ship->getShip()->getId();?>&fleetId=<?php echo $fleet->getId();?>" class="btn btn-danger btn-sm">Remove</a>
+                <a href="/manage/fleets/edit.php?shipId=<?php echo $ship_fleet->getShip()->getId();?>&fleetId=<?php echo $fleet->getId()?>" class="btn btn-success btn-sm">Edit</a>
+                <a href="/manage/fleets/removeShip.php?shipId=<?php echo $ship_fleet->getShip()->getId();?>&fleetId=<?php echo $fleet->getId();?>" class="btn btn-danger btn-sm">Remove</a>
             </td>
         </tr>
     <?php endforeach;?>
@@ -50,7 +49,7 @@ else:
 
 <div class='text-center'>
     <?php if (count($ships) != count($fleet->getShipFleets())):?>
-        <a href="/manage/fleets/addShip.php?id=<?php echo $fleet->getId();?>&team=<?php echo $fleet->getTeam();?>" class="btn btn-success">Add Ship</a>
+        <a href="/manage/fleets/addShip.php?id=<?php echo $fleet->getId();?>" class="btn btn-success">Add Ship</a>
     <?php else:?>
         <button class='btn btn-success' disabled>Add Ship</button>
     <?php endif;?>
